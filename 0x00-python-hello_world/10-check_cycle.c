6b9f703537c5600c681642ef3c1085880f673905
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-03 00:00:08 -0500

    check cycle in linked list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,5 +8,20 @@
 */
 int check_cycle(listint_t *list)
 {
+	listint_t *slow_p, *fast_p;
+
+	if (slow_p == NULL || fast_p == NULL)
+		return (0);
+
+	slow_p = list->next;
+	fast_p = list->next->next;
+
+	while (slow_p && fast_p && fast_p->next)
+	{
+		slow_p = slow_p->next;
+		fast_p = fast_p->next->next;
+		if (slow_p == fast_p)
+			return (1);
+	}
 	return (0);
 }
