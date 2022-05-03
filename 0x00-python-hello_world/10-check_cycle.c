fbbae35d35c39245e0c822742fb1b28e470bda52
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-02 23:04:46 -0500

    just the file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include "list.h"
+/**
+  * check_cycle - Function that check if a
+  * singly linked list has a cycle in it
+  * @list: pointer to a list
+  * Return: 0 if there is no cycle, 1 if there
+  * is a cycle
+*/
+int check_cycle(listint_t *list)
+{
+	return (0);
+}
