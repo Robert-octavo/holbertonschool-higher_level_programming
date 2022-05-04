e014ebff78370fd663762f43d48766380ffef637
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-03 21:51:04 -0500

    correct pycodestyle :( :( :(

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 10):
-    for number2 in range (number + 1, 10):
+    for number2 in range(number + 1, 10):
         if number == 8 and number2 == 9:
             print(f"{number}{number2}")
         else:
