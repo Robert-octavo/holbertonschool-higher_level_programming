cf11414dd738214e0a099bbd469982ba7487cdb1
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-05 20:50:50 -0500

    correct the space in range

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     narg = len(sys.argv) - 1
     sum = 0
-    for i in range (narg):
+    for i in range(narg):
         sum = sum + int(sys.argv[i + 1])
     print(f"{sum}")
