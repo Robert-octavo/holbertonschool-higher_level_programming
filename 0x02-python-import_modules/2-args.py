2c665d3bf67ae13b2240df727943fbd377521c6f
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-05 20:23:06 -0500

    print the number and the list

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,5 +2,11 @@
 if __name__ == "__main__":
     import sys
     narg = len(sys.argv) - 1
-    print(f"{narg} arguments")
+    if narg == 0:
+        print(f"{narg} arguments.")
+    else:
+        print(f"{narg} arguments:")
+    for i in range(narg):
+        print(f"{i + 1}: {sys.argv[i + 1]}")
+
 
