d8ec869d674bf83978ba816ca1c99a1ad389c4be
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-05 21:29:23 -0500

    test prints all the names defined by the compiled

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    import sys
+    #returns all properties and methods of the specified object
+    n = dir(hidden_4)
+    print(f"{n[0]}")
+    for i in n:
+        if i[:2] != "__":
+            print(f"{i}")
