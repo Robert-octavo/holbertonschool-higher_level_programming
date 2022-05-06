2f76bdbd85d9c4364c637d835accfd62020437b6
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-05 20:37:48 -0500

    add a different output when it's 1 argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     narg = len(sys.argv) - 1
     if narg == 0:
         print(f"{narg} arguments.")
+    elif narg == 1:
+        print(f"{narg} argument:")
     else:
         print(f"{narg} arguments:")
     for i in range(narg):
