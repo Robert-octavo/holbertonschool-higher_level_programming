f07b76df7a5fd715c7d96a4436cb410e7d2fb0c1
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-09 22:06:05 -0500

    Erase the test part

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,13 +5,4 @@ def print_matrix_integer(matrix=[[]]):
         for j in range(len(matrix[i])):
             print(f"{matrix[i][j]}", end="")
         print("$")
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
+        
