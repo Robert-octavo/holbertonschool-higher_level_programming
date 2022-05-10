a583298c4237672e619d2c4c65243c3531ad46ad
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-09 23:32:10 -0500

    correct pycostyle and erase the test part

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -8,7 +8,3 @@ def max_integer(my_list=[]):
         if my_list[i] > max:
             max = my_list[i]
     return (max)
-
-my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
