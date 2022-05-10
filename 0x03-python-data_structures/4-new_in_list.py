d7562b19ff3771d8e6f83d640e9e97f0185df68f
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-09 21:21:36 -0500

    correct pycodestyle

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,7 +7,7 @@ def new_in_list(my_list, idx, new_element):
 
     my_new_list = []
     my_new_list.extend(my_list)
-    #print(my_new_list)
+
     my_new_list[idx] = new_element
 
     return (my_new_list)
