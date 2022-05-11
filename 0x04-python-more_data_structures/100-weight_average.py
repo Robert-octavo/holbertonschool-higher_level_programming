98a059002c0edbc8a2b5d6745846ee5c7de428f3
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-11 00:23:35 -0500

    erase the test part

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -7,8 +7,3 @@ def weight_average(my_list=[]):
         average += (i[0] * i[1])
         size += i[1]
     return (average / size)
-
-my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
-# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
-result = weight_average(my_list)
-print("Average: {:0.2f}".format(result))
