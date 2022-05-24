b4b97985e8de2f0197f03d4dcfb901c7fa298c25
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-23 19:52:55 -0500

    correct message output

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -15,5 +15,5 @@ def safe_print_division(a, b):
     except (ZeroDivisionError, TypeError):
         division = None
     finally:
-        print("Inside Result: {}".format(division))
+        print("Inside result: {}".format(division))
     return (division)
