ca777427bae192d3986f1f68360f5f740d19a3d7
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-23 21:16:46 -0500

    correct output

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@
 Write a function that divides element by element 2 lists.
 - my_list_1 and my_list_2 can contain any type (integer, string, etc.)
 - list_length can be bigger than the length of both lists
-- Returns a new list (length = list_length) with all divisions
+- Returns  a new list (length = list_length) with all divisions
 - If 2 elements can not be divided, the division result should be equal to 0
 - If an element is not an integer or float:
     print: wrong type
