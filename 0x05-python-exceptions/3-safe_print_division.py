ef14202ba5dd904c1cfbefa87ab7fa0cc73e36bc
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-23 19:51:18 -0500

    Correct and test Safe Ptint Division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-safe_print_division = __import__('3-safe_print_division').safe_print_division
+"""
+Write a function that divides 2 integers and prints the result.
+- You can assume that a and b are integers
+- The result of the division should print on the finally: section
+preceded by Inside result:
+- Returns the value of the division, otherwise: None
+- You have to use try: / except: / finally:
+"""
 
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
 
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
+def safe_print_division(a, b):
+    try:
+        division = a / b
+    except (ZeroDivisionError, TypeError):
+        division = None
+    finally:
+        print("Inside Result: {}".format(division))
+    return (division)
