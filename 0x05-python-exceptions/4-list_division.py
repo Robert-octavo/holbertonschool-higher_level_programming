91c56dd2d71500d3ff8393237ccbe129820cde91
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-23 21:22:40 -0500

    correct output Division and Wrong

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -20,13 +20,13 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             division = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            print("Division by 0")
+            print("division by 0")
             division = 0
         except IndexError:
             print("out of range")
             division = 0
         except TypeError:
-            print("Wrong Type")
+            print("wrong type")
             division = 0
         finally:
             my_list_3.append(division)
