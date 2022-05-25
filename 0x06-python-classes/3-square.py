ce08dbf1bc2f1a5f9c6fe9bb7616bcf9c327ef7f
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-24 22:30:20 -0500

    Methon that returns the scuare area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Write a class Square that defines a square by: (based on 2-square.py)
+
+    - Private instance attribute: size
+    - Instantiation with optional size: def __init__(self, size=0):
+        - size must be an integer, otherwise raise a TypeError exception
+        with the message size must be an integer
+        - if size is less than 0, raise a ValueError exception with the
+        message size must be >= 0
+    - Public instance method: def area(self): that returns the current square
+    area
+"""
+
+
+class Square:
+    """
+    The isinstance() function returns True if the specified object
+    is of the specified type, otherwise False.
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+
