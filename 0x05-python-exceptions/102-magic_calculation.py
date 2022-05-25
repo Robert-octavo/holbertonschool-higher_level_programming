7c8da3fd87902f708f467d81680add9810cb5314
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-24 19:27:20 -0500

    else part missing

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -60,4 +60,12 @@ Magic calcultation.
 
 def magic_calculation(a, b):
     result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+        except:
+            result = b + a
+            break
     return (result)
