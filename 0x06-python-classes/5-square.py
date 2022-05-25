335709050fe3e2ed2ac4e7d82852cf1c82d87b1a
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-24 23:16:46 -0500

    Correct pycodestyle

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -10,8 +10,10 @@ Write a class Square that defines a square by: (based on 4-square.py)
             - if size is less than 0, raise a ValueError exception with the
             message size must be >= 0
     - Instantiation with optional size: def __init__(self, size=0):
-    - Public instance method: def area(self): that returns the current square area
-    - Public instance method: def my_print(self): that prints in stdout the square with
+    - Public instance method: def area(self): that returns the current square
+    area
+    - Public instance method: def my_print(self): that prints in stdout the
+    square with
     the character #:
         - if size is equal to 0, print an empty line
 
@@ -46,7 +48,7 @@ class Square:
     def area(self):
         """Return the current area"""
         return (self.__size * self.__size)
-    
+
     def my_print(self):
         for i in range(0, self.__size):
             for j in range(self.__size):
