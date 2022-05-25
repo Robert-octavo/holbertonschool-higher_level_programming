4bfca51c7f03c037ba7d2926a9f8bb9a220f1b7b
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-24 21:00:57 -0500

    correct pycodestyle again

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,13 +6,13 @@ Write a class Square that defines a square by: (based on 1-square.py)
     - Instantiation with optional size: def __init__(self, size=0):
         - size must be an integer, otherwise raise a TypeError exception
         with the message size must be an integer
-        - if size is less than 0, raise a ValueError exception with the 
+        - if size is less than 0, raise a ValueError exception with the
         message size must be >= 0
 """
 
 
 class Square:
-    """ 
+    """
     The isinstance() function returns True if the specified object
     is of the specified type, otherwise False.
     """
