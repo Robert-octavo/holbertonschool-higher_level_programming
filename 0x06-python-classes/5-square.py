e320850e42c5d69221d482f93029eb059d76084c
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-24 23:12:20 -0500

    Method my_print(self)

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Write a class Square that defines a square by: (based on 4-square.py)
+
+    - Private instance attribute: size:
+        - property def size(self): to retrieve it
+        - property setter def size(self, value): to set it:
+            - size must be an integer, otherwise raise a TypeError exception
+            with the message size must be an integer
+            - if size is less than 0, raise a ValueError exception with the
+            message size must be >= 0
+    - Instantiation with optional size: def __init__(self, size=0):
+    - Public instance method: def area(self): that returns the current square area
+    - Public instance method: def my_print(self): that prints in stdout the square with
+    the character #:
+        - if size is equal to 0, print an empty line
+
+"""
+
+
+class Square:
+    """
+    The isinstance() function returns True if the specified object
+    is of the specified type, otherwise False.
+    """
+    def __init__(self, size=0):
+        self.__size = size
+    """In Python property()is a built-in function that creates and
+        returns a property object. A property object has three methods,
+        getter(), setter(), and delete()
+        https://www.geeksforgeeks.org/getter-and-setter-in-python/
+    """
+    @property
+    def size(self):
+        """Return the current size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Return the current area"""
+        return (self.__size * self.__size)
+    
+    def my_print(self):
+        for i in range(0, self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
+        if self.__size == 0:
+            print("")
