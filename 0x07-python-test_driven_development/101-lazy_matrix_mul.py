4acc147594929a1da42232be000759578c28b62e
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-31 14:17:56 -0500

    correct pycodestyle

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,10 +5,12 @@ Write a function that multiplies 2 matrices by using the module NumPy
 To install it: pip3 install numpy==1.15.0
 
     - Prototype: def lazy_matrix_mul(m_a, m_b):
-    - Test cases should be the same as 100-matrix_mul but with new exception type/message
+    - Test cases should be the same as 100-matrix_mul but with new
+    exception type/message
 
 """
 from numpy import matmul
+
 
 def lazy_matrix_mul(m_a, m_b):
     """Multiplies 2 matrices by using Numpy"""
