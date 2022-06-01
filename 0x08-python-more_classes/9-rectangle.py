131e025d271a7514af5ae66b92ce15307e526f6d
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-01 00:09:59 -0500

    correct pycodestyle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -45,8 +45,8 @@ Write a class Rectangle that defines a rectangle by:
         - rect_2 must be an instance of Rectangle, otherwise raise a TypeError
         exception with the message rect_2 must be an instance of Rectangle
         - Returns rect_1 if both have the same area value
-    -Class method def square(cls, size=0): that returns a new Rectangle instance
-    with width == height == size
+    -Class method def square(cls, size=0): that returns a new Rectangle
+    instance with width == height == size
 You are not allowed to import any module
 
 """
