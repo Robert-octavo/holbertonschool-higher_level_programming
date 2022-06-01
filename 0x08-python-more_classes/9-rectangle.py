8c1a6397475acd974a8bf0b0b59b48eff79b6291
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-31 23:54:28 -0500

    Rectangle instance with width == height == size

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -131,3 +131,8 @@ class Rectangle:
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         return rect_1 if (rect_1.area() >= rect_2.area()) else rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """new Rectangle instance with width == height == size"""
+        return (cls(size, size))
