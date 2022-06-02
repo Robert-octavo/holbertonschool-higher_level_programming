c678483c8591a8bddc8771725dd9cf786aee4a0c
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-01 19:41:59 -0500

    prevents the user from creating new instance

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Write a class LockedClass with no class or object attribute,
+that prevents the user from dynamically creating new instance
+attributes, except if the new instance attribute is called
+first_name.
+https://stackoverflow.com/questions/3603502/prevent-creating-new-attributes-outside-init
+"""
+
+
+class LockedClass:
+    __slots__ = ["first_name"]
+
+    def __init__(self, first_name=""):
+        self.first_name = first_name
