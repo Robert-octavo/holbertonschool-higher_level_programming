c1bcbf6dec87c5964ad51a9825fee71ddbce58c5
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-07 16:11:15 -0500

    using dir to return the list of attributes

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Write a function that returns the list of available attributes and methods of an object:
+
+    - Prototype: def lookup(obj):
+    - Returns a list object
+    - You are not allowed to import any module
+
+
+"""
+
+
+def lookup(obj):
+    """The dir() method returns the list of valid attributes of the passed object"""
+    return dir(obj)
