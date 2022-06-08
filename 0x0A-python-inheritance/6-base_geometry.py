9dc66d5a068ae705558adbaa10097c69d36dc795
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-07 20:46:20 -0500

    def area with Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Write a class BaseGeometry (based on 5-base_geometry.py).
+
+    - Public instance method: def area(self): that raises
+    an Exception with the message area() is not implemented
+    - You are not allowed to import any module
+
+"""
+
+
+class BaseGeometry():
+    """class Base Geometry"""
+    def area(self):
+        raise Exception ("area() is not implemented ")
