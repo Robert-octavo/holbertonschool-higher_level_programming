5531b04823ea9e2db22468f8ac08d30bc1e3955d
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-07 19:24:20 -0500

    same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Write a function that returns True if the object
+is exactly an instance of the specified class;
+otherwise False.
+
+    - Prototype: def is_same_class(obj, a_class):
+    - You are not allowed to import any module
+
+"""
+
+
+def is_same_class(obj, a_class):
+    return True if isinstance(obj, a_class) else False
