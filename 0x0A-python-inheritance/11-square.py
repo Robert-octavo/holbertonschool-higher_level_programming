2da7c8722ed1517fa834bd703430c618ccc80e1e
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-07 23:03:11 -0500

    implement the str

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Write a class Square that inherits from Rectangle (9-rectangle.py):
+
+    - Instantiation with size: def __init__(self, size)::
+        - size must be private. No getter or setter
+        - size must be a positive integer, validated by integer_validator
+    - the area() method must be implemented
+
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(Rectangle):
+    """class Rectangle inherits from Basegeometry"""
+    def __init__(self, size):
+        """Function to Initializes an instance"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """formated string"""
+        return str(f"[Square] {self.__size}/{self.__size}")
+
+    def area(self):
+        """Square's area"""
+        return (self.__size * self.__size)
