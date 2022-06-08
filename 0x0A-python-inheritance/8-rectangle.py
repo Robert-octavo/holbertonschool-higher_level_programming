2e00eb6c8c0b9e58ef14b12d13c3df01c2b06df2
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-07 22:06:46 -0500

    Rectangle inherits from BaseGeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Write a class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
+
+    - Instantiation with width and height: def __init__(self, width, height):
+        - width and height must be private. No getter or setter
+        - width and height must be positive integers, validated by
+        integer_validator
+
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """class Rectangle inherits from Basegeometry"""
+    def __init__(self, width, height):
+        """Function to Initializes an instance"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.width = width
+        self.height = height
