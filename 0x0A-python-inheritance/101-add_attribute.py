fa84ea6444b86b90f9f57cb58f01d9648382b4c5
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-08 00:26:29 -0500

    add attribute

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Write a function that adds a new attribute to an object if it is possible:
+
+    - Raise a TypeError exception, with the message can't add new attribute
+    if the object cannot have new attribute
+    - You are not allowed to use try/except
+    - You are not allowed to import any module
+
+"""
+
+
+def add_attribute(obj, new_attr, attr_val):
+    """adds a new attribute to an object if it is possible"""
+    err = "can't add new attribute"
+    if not hasattr(obj, "__dict__"):
+        raise TypeError(err) 
+    else: 
+        setattr(obj, new_attr, attr_val)
