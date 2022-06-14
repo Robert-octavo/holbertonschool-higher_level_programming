d219e1faa254da53f20d7312d3806c5201e5b2be
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-14 17:16:13 -0500

    correct pycodestyle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,11 +9,12 @@ import json
 class Base():
     """class Base"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects +=1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
     @staticmethod
@@ -39,8 +40,8 @@ class Base():
 
     @staticmethod
     def from_json_string(json_string):
-        """static method that returns the list of the JSON string representation
-        json_string"""
+        """static method that returns the list of the JSON string
+        representation json_string"""
         empty_list = []
         if json_string is None or json_string == "[]":
             return empty_list
