830f203c25870afcdc564debbc75bf7513b04878
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-13 23:33:52 -0500

    add stactic method using loads and to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """Create a Class Base: """
 
 
+from encodings import utf_8
 import json
 
 
@@ -20,6 +21,27 @@ class Base():
         """static method that returns the JSON string representation of
         list_dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
-            return []
+            return "[]"
         else:
             return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """class method that writes the JSON string representation of
+        list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        with open(filename, "w", encoding="utf_8") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list = [object.to_dictionary() for object in list_objs]
+                file.write(Base.to_json_string(list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """static method that returns the list of the JSON string representation
+        json_string"""
+        empty_list = []
+        if json_string is None or json_string == "[]":
+            return empty_list
+        return json.loads(json_string)
