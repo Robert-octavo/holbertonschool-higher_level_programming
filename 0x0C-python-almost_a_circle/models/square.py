333defe7defb4b8b6fb91e192c9f23e41f3318ba
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-14 17:58:59 -0500

    correct pycodestyle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,6 +2,7 @@
 """Class Rectangle"""
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """class square"""
     def __init__(self, size, x=0, y=0, id=None):
@@ -22,37 +23,37 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
-            """public method that assigns an argument to each attribute"""
-            if args and len(args) != 0:
-                j = 0
-                for i in args:
-                    if j == 0:
-                        self.id = i
-                    elif j == 1:
-                        self.size = i
-                    elif j == 2:
-                        self.x = i
-                    elif j == 3:
-                        self.y = i
-                    j = j + 1
-            elif kwargs and len(kwargs) != 0:
-                """ that assigns a key/value argument to attributes:"""
-                for key, value in kwargs.items():
-                    if key == "id":
-                        self.id = value
-                    elif key == "size":
-                        self.size = value
-                    elif key == "x":
-                        self.x = value
-                    elif key == "y":
-                        self.y = value
+        """public method that assigns an argument to each attribute"""
+        if args and len(args) != 0:
+            j = 0
+            for i in args:
+                if j == 0:
+                    self.id = i
+                elif j == 1:
+                    self.size = i
+                elif j == 2:
+                    self.x = i
+                elif j == 3:
+                    self.y = i
+                j = j + 1
+        elif kwargs and len(kwargs) != 0:
+            """ that assigns a key/value argument to attributes:"""
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
 
     def to_dictionary(self):
         """that returns the dictionary representation of a Rectangle"""
         """Retur a Key/value"""
-        dictionary = {"id": self.id, 
-                    "height": self.height,
-                    "x": self.x,
-                    "y": self.y 
-                    }
+        dictionary = {
+                        "id": self.id,
+                        "height": self.height,
+                        "x": self.x,
+                        "y": self.y}
         return dictionary
