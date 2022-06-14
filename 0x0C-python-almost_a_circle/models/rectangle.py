37507a8597cc86179725d0220792c04cf8907027
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-14 17:34:10 -0500

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -55,6 +55,8 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
