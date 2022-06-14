3af874195430d54be978879c855d5805739ea989
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-13 21:20:49 -0500

    return JSON string representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """Create a Class Base: """
 
 
+import json
+
+
 class Base():
     """class Base"""
     __nb_objects = 0
@@ -11,3 +14,12 @@ class Base():
         else:
             Base.__nb_objects +=1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """static method that returns the JSON string representation of
+        list_dictionaries"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return []
+        else:
+            return (json.dumps(list_dictionaries))
