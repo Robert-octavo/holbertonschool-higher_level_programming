a9005c9fd6979ff7b01131f734c04dc1b797d172
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-08-15 01:06:31 -0500

    add message for more than one argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const args = process.argv.length;
 // console.log(args)
-console.log(args === 2 ? 'No argument' : 'Argument found');
+// console.log(args === 2 ? 'No argument' : 'Argument found');
+console.log(args === 2 ? 'No argument' : args === 3 ? 'Argument found' : 'Arguments found');
