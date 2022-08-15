9306d8cbc3a0d7dafe08e8bb18c55eb11e1f9924
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-08-15 17:57:24 -0500

    correct semistandar format

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const str = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let j = 0; j < str.length; j++)
-    console.log(str[j]);
+for (let j = 0; j < str.length; j++) {
+  console.log(str[j]);
+}
