7c615d2543c4105619d915dbe73922ab27386a0f
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-08-16 12:42:16 -0500

    change the var to const

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 // TODO: Write a function that returns the reversed version of a list:
 
 exports.esrever = function (list) {
-  var output = [];
+  const output = [];
   while (list.length) {
     output.push(list.pop());
   }
