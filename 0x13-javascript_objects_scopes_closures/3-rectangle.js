0fbb601aa65ab8dd0f55a1510f3e8fc81274f471
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-08-16 10:30:22 -0500

    instance method called print()

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// TODO: If w or h is equal to 0 or not a positive integer, create an empty object
+// TODO: Create an instance method called print() that prints the rectangle using the character X
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
