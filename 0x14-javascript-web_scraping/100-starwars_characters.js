f5f59e503d04a8ccb1d6fe68e92fb2f6d47e042d
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-08-17 15:26:07 -0500

    script that computes the number of tasks completed by user id.

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+// TODO: https://github.com/axios/axios
+// TODO: sudo npm install --save axios
+
+const axios = require('axios').default;
+
+axios.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/')
+  .then(function (response) {
+    // handle success
+    // console.log(response.data.characters[0]);
+    // console.log(response.data.title);
+    for (let i = 0; response.data.characters[i]; i++) {
+      // console.log(response.data.characters[i])
+      axios.get(response.data.characters[i])
+        .then(function (rest) {
+          // console.log(rest.data)
+          for (const [key, value] of Object.entries(rest.data)) {
+            if (key === 'name') {
+              console.log(value);
+            }
+          }
+        });
+    }
+  }).catch(error => {
+    console.log(error.response.status);
+  });
