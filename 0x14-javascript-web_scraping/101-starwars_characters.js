9160c84f12bf84f4ba8eaa4dd648a744bb410e7a
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-08-17 17:57:44 -0500

    Another one :(

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 // TODO: https://github.com/axios/axios
 // TODO: sudo npm install --save axios
+
+const { link } = require('fs');
+
 // let order = [];
 const axios = require('axios').default;
 
@@ -9,19 +12,17 @@ axios.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/')
     // handle success
     // console.log(response.data.characters[0]);
     // console.log(response.data.title);
-    for (let i = 0; response.data.characters[i]; i++) {
-      // console.log(response.data.characters[i])
-      axios.get(response.data.characters[i])
-        .then(function (rest) {
-          // console.log(rest.data)
-          // let j = 0;
-          for (const [key, value] of Object.entries(rest.data)) {
-            if (key === 'name') {
-              console.log(value);
-            }
-          }
+    // for (let i = 0; response.data.characters[i]; i++) {
+    // console.log(response.data.characters[i])
+    response.data.characters.forEach(link => {
+      axios.get(link)
+        .then(function (response) {
+          // console.log(link);
+          console.log(response.data.name);
         });
-    }
+    });
+
+    // }
   }).catch(error => {
     console.log(error.response.status);
   });
