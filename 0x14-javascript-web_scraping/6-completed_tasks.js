c9f0064a329f915e4a13a05738ebd134d09ef36a
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-08-17 13:51:53 -0500

    test

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+// TODO: Write a script that gets the contents of a webpage and stores it in a file.
+
+const axios = require('axios').default;
+
+axios.get(process.argv[2])
+  .then(function (response) {
+    // handle success
+    // console.log(response)
+    // console.log(response.data);
+    const count = {};
+    let num = 0;
+    for (let i = 0; response.data[i]; i++) {
+      if (response.data[i].completed === true) {
+        // count[response.data[i].userId] = num;
+        // num++;
+        if (isNaN(count[response.data[i].userId])) {
+          count[response.data[i].userId] = 0;
+        } else {
+          count[response.data[i].userId] += 1;
+        }
+      }
+    }
+    console.log(count);
+  });/* .catch(error => {
+    console.log(error.response.status);
+  }); */
