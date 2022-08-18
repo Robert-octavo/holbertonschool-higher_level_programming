f2b32b2770e0534f141b31cf0332c5f64ce8e260
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-08-17 19:02:47 -0500

    a lot of changes - another try

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,19 +6,20 @@ const axios = require('axios').default;
 
 axios.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/')
   .then(function (response) {
-    // handle success
-    // console.log(response.data.characters[0]);
-    // console.log(response.data.title);
-    // for (let i = 0; response.data.characters[i]; i++) {
-    // console.log(response.data.characters[i])
-    response.data.characters.forEach(data => {
-      axios.get(data)
-        .then(function (response) {
-          console.log(response.data.name);
-        });
-    });
-
-    // }
+    const characters = response.data.characters;
+    // console.log(characters);
+    printCar(characters, 0);
   }).catch(error => {
     console.log(error.response.status);
   });
+
+function printCar (characters, index) {
+  // console.log(characters + ' + ' + index);
+  axios.get(characters[index])
+    .then(function (response) {
+      console.log(response.data.name);
+      if (index + 1 < characters.length) {
+        printCar(characters, index + 1);
+      }
+    });
+}
