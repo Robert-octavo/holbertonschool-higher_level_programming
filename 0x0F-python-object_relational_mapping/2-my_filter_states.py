91dca2e10adfd9fee82f508a2095328105288433
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-08-30 01:00:49 -0500

    test with like

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,13 +4,13 @@ the states table of hbtn_0e_0_usa where name matches the argument.
 """
 
 import sys
-from unittest import result
 import MySQLdb
 
 if __name__ == "__main__":
+    # db = MySQLdb.connect(user=sys.argv[1], db=sys.argv[2])
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name ='{}'".format(sys.argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'".format(sys.argv[4]))
     # result = cursor.fetchone()
     # print(result)
     [print(state) for state in cursor.fetchall()]
