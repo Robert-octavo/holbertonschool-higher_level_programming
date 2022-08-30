c0e8baa56d5f5e6e563e84ec062c6933323ee3ab
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-08-30 00:15:50 -0500

    Write a script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Write a script that lists all states from the database hbtn_0e_0_usa
+
+import sys
+import MySQLdb
+
+# TODO: Create a connection getting the param from the terminal and with cursor
+# allow me to execute a query, fetchall() allow me to get all the data. fetchmany(), fetchone()
+
+db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+cursor = db.cursor()
+cursor.execute("SELECT * FROM `states`")
+[print(state) for state in cursor.fetchall()]
