2f95c4179cc99827199e03dd0d47d9e94bbc2126
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-08-30 00:28:00 -0500

    pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,10 +5,14 @@ import sys
 import MySQLdb
 
 # TODO: Create a connection getting the param from the terminal and with cursor
-# allow me to execute a query, fetchall() allow me to get all the data. 
+# allow me to execute a query, fetchall() allow me to get all the data.
 # fetchmany(), fetchone()
 
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+db = MySQLdb.connect(
+                    host="localhost", port=3306,
+                    user=sys.argv[1], passwd=sys.argv[2],
+                    db=sys.argv[3]
+                    )
 cursor = db.cursor()
 cursor.execute("SELECT * FROM `states`")
 [print(state) for state in cursor.fetchall()]
