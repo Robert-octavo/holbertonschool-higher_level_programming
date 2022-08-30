e4617c039c5f19ff8aee87eb5d25dd63a718368e
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-08-30 02:54:32 -0500

    test with sessionMaker

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
         'mysql+mysqldb://{}:{}@localhost/{}'.
         format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-    Base.metadata.create_all(engine)
-    session = Session(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
     for city in session.query(City).order_by(City.id):
         print(f"{city.id}: {city.name} -> {city.state.name}")
