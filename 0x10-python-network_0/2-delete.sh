fb5f89355351422f6a9f18f01bbb47e43252b8aa
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-01 02:59:50 -0500

    Write a Bash script that sends a DELETE request to the URL passed

---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Write a Bash script that sends a DELETE request to the URL passed
+# as the first argument and displays the body of the response
+# https://reqbin.com/req/c-1dw4uds4/curl-delete-request-example
+curl -s "$1" -X DELETE
