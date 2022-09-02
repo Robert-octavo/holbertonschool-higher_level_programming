cfe44b7d0b566a210807865cae064fa8314e45da
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-02 10:05:26 -0500

    test

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,5 +2,6 @@
 # Bash script that takes in a URL, sends a request
 # to that URL, and displays the size of the body of
 # the response
-# https://stackoverflow.com/questions/4497759/how-to-get-remote-file-size-from-a-shell-script
+# https://stackoverflow.com/questions/4497759/how-to
+# -get-remote-file-size-from-a-shell-script
 curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
