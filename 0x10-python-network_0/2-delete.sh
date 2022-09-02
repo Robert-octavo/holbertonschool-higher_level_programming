a6465aa6c14148b62bc23c263f12b0f5eed6ddfd
Robert Ortega <98441105+Robert-octavo@users.noreply.github.com>
2022-09-02 10:18:45 -0500

    Update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Write a Bash script that sends a DELETE request to the URL passed
-# as the first argument and displays the body of the response
-# https://reqbin.com/req/c-1dw4uds4/curl-delete-request-example
+# Write a Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response https://reqbin.com/req/c-1dw4uds4/curl-delete-request-example
 curl -s "$1" -X DELETE
