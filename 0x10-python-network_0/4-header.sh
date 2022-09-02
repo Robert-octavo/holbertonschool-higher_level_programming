e651cfc325e95258b4d01d27e850ad20842780db
Robert Ortega <98441105+Robert-octavo@users.noreply.github.com>
2022-09-02 10:19:31 -0500

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Write a Bash script that takes in a URL as an argument, sends a GET request
-# to the URL, and displays the body of the response
-# https://akrabat.com/view-header-and-body-with-curl/
-# https://curl.se/docs/manpage.html --header section
-
+# Write a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
 curl -s -X GET --header "X-HolbertonSchool-User-Id: 98" "$1"
