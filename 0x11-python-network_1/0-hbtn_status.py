c0adce2b4bad0ab35a754bc008043fb9372dba8e
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-02 11:06:44 -0500

    test

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,8 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as res:
+    # request = urllib.request.Request()
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as res:
         html = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
