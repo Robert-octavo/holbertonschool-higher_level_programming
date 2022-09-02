14472fe05f05b5c46b70e2272026271c358ca3ff
Robert Ortega <98441105+Robert-octavo@users.noreply.github.com>
2022-09-02 10:15:08 -0500

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Write a Bash script that sends a request to a URL passed as an argument,
-# and displays only the status code of the response.
-# https://superuser.com/questions/272265/getting-curl-to-output-http-status-code
-# https://curl.se/docs/manpage.html  look for --write-out section
-# -o /dev/null To suppress response bodies, you can redirect output to /dev/null
-
+# Write a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
 curl -s -o /dev/null --write-out "%{http_code}" "$1"
