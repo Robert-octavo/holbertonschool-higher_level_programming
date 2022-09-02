410b2c4d0febc31e755fe10b012da9df2df8062d
Robert Ortega <98441105+Robert-octavo@users.noreply.github.com>
2022-09-02 10:19:57 -0500

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Write a Bash script that takes in a URL, sends a POST request to
-# the passed URL, and displays the body of the response
-# https://akrabat.com/view-header-and-body-with-curl/
-# https://curl.se/docs/manpage.html  look for --data section
-
+# Write a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
 curl -s -X POST --data "email=test@gmail.com" --data "subject=I will always be here for PLD" "$1"
