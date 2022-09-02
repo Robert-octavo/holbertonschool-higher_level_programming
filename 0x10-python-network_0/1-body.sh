603abfe33cfde172e9ee5b7eb16fb3507678752f
Robert Ortega <98441105+Robert-octavo@users.noreply.github.com>
2022-09-02 10:13:40 -0500

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Write a Bash script that takes in a URL, sends a GET request
-# to the URL, and displays the body of the response
-# https://reqbin.com/req/c-1n4ljxb9/curl-get-request-example
+# Write a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response https://reqbin.com/req/c-1n4ljxb9/curl-get-request-example
 curl -s "$1" -X GET -L echo""
