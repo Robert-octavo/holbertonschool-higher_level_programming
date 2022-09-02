53ea4eaa3d7391a15b12003f4a77bace98e56c15
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-02 15:25:29 -0500

    test

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
         with request.urlopen(sys.argv[1]) as res:
             print(res.read().decode('utf-8'))
     except error.HTTPError as e:
-        print(f'Error code: {e.code}')
+        print('Error code: {}'.format(e.code))
