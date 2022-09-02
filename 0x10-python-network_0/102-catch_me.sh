823d271a1c0070d684af5ae5360ebb9191e05c50
Robert Ortega <98441105+Robert-octavo@users.noreply.github.com>
2022-09-02 10:17:02 -0500

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Write a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-URL="0.0.0.0:5000/catch_me"
-curl -sL -X PUT --header 'Origin: HolbertonSchool' --data "user_id=98" "$URL"
+curl -sL -X PUT --header 'Origin: HolbertonSchool' --data "user_id=98" "0.0.0.0:5000/catch_me"
