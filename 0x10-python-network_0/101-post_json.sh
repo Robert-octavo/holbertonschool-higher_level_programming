29a05f544444bfd28c26d8744ff35ad52870c85b
Robert Ortega <98441105+Robert-octavo@users.noreply.github.com>
2022-09-02 10:15:38 -0500

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Write a Bash script that sends a JSON POST request to a URL passed as the first argument,
-# and displays the body of the response.
-# https://reqbin.com/req/c-dwjszac0/curl-post-json-example
-# curl -X POST https://reqbin.com/echo/post/json -d @filename  @ add a file name
-
+# Write a Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
 curl -s -X POST --header 'Content-Type: application/json' --data @"$2" "$1"
