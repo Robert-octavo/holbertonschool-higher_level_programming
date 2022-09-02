c4aac32f78b53e8abb1a213717e3075b899d82f8
Robert Ortega <98441105+Robert-octavo@users.noreply.github.com>
2022-09-02 10:19:09 -0500

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Write a Bash script that takes in a URL and displays
-# all HTTP methods the server will accept.
-# https://stackoverflow.com/questions/42549146/how-do-you-find-the-list-of-all-the-supported-http-methods-by-a-webserver
-# https://linuxhint.com/bash-cut-command-examples/
+# Write a Bash script that takes in a URL and displays all HTTP methods the server will accept.
 curl -sI -X OPTIONS "$1" | grep -i Allow: | cut -d " " -f 2-
