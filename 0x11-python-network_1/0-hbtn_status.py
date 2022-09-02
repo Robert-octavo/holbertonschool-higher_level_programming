1c542d4e5cae0365da2ece6f1462e8abbf99c7a4
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-02 10:46:25 -0500

    test

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Python script that fetches https://intranet.hbtn.io/status"""
-import urllib.request as re
+import urllib.request
 
 
 if __name__ == "__main__":
-    with re.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
         print(f'\t- type: {type(html)}')
